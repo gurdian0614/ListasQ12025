80f79e60e3b9dfdeffe46e30b1f05f150101bd27
gurdian0614 <ivan.gurdian@outlook.com>
2025-02-05 18:56:44 -0600

    Add project files.

---
+++ ListasQ12025/Program.cs
@@ -0,0 +1,19 @@
+﻿
+List<string> listaString = new List<string>();
+listaString.Add("Naranja");
+listaString.Add("5");
+listaString.Add("Real España");
+listaString.Add("Barcelona");
+listaString.Add("Real Madrid");
+listaString.Add("Real Madrid");
+listaString.Add("Alajuela");
+listaString.Insert(0, "Real Madrid");
+//listaString.Remove("Real Madrid");
+//listaString.RemoveAt(5); // Tener cuidado de no agregar un indice fuera del rango
+//listaString.RemoveAll(lista => lista.Contains("Real Madrid"));
+
+foreach (string item in listaString)
+{
+    //Console.WriteLine($"{listaString.IndexOf(item)}: {item}");
+    Console.WriteLine($"Item: {item}");
+}
